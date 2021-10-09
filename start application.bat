2eb3790cf53b233e19d1d0f238778228264752a6
AkshayaKumarKR <akskukr@gmail.com>
2021-10-09 16:16:22 +0200

    Start bat file and abstract

---
+++ start application.bat
@@ -0,0 +1,10 @@
+start /d "%cd%\csharp\UltrahapticsShapes\bin\x64\Debug" UltrahapticsShapes.exe
+
+start http://localhost:3000/
+
+cmd /c "cd /d %cd%\JS\ultrahaptics-ejs && npm install && npm run dev"
+
+
+
+
+
